62152881a5929946a0aa783dc7e27c131027f9c0
Abu Siam <siamcse01@gmail.com>
2022-11-08 11:07:56 +0600

    banner added

---
+++ src/pages/Home/Banner/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Banner = () => {
+    return (
+        <div className="hero h-96" style={{ backgroundImage: `url("https://plus.unsplash.com/premium_photo-1661692645616-503901050ea3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")` }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-md">
+                    <h1 className="mb-5 text-5xl font-bold">Home Made Food at your doorstep</h1>
+                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/pages/Home/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+
+const Home = () => {
+    
+    return (
+        <div className='lg:max-w-screen-xl mx-auto'>
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Home;
