bb23765f39d8f20bf6d79604966d0ae571fc2e46
Abu Siam <siamcse01@gmail.com>
2022-11-08 12:28:05 +0600

    context added

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,15 +1,47 @@
-import React, { createContext } from 'react';
-import { getAuth } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
-const AuthProvider = () => {
-    return (
-        <div>
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState({});
+    const [loading, setLoading] = useState(true);
 
-        </div>
+    const signUp = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const popupSignIn = (provider) => {
+        setLoading(true);
+        return signInWithPopup(auth, provider);
+    }
+
+    const logOut = () => {
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+            console.log('state changed', currentUser);
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return ()=> unSubscribe();
+    }, [])
+
+    const authInfo = { user, loading, signUp, signIn, popupSignIn, logOut }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 
--- src/pages/Login/Register.js
+++ src/pages/Login/Register.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthProvider';
 
 const Register = () => {
+    const {signUp} = useContext(AuthContext);
+
+    const handleSubmit=event=>{
+        event.preventDefault();
+
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name,email,password);
+
+    }
     return (
         <div className='flex items-center justify-center'>
             <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-base-200 text-gray-900">
                 <h1 className="text-2xl font-bold text-center">Registration</h1>
-                <form  className="space-y-6 ng-untouched ng-pristine ng-valid">
+                <form onSubmit={handleSubmit}  className="space-y-6 ng-untouched ng-pristine ng-valid">
                     <div className="space-y-1 text-sm">
                         <label for="username" className="block text-gray-800">Name</label>
                         <input type="text" name="name" id="name" placeholder="Name" className="w-full px-4 py-3 rounded-md border-gray-700 focus:border-violet-400" />
                     </div>
                     <div className="space-y-1 text-sm">
                         <label for="username" className="block text-gray-800">Email</label>
-                        <input type="text" name="username" id="username" placeholder="Username" className="w-full px-4 py-3 rounded-md border-gray-700 focus:border-violet-400" />
+                        <input type="text" name="email" id="email" placeholder="Email" className="w-full px-4 py-3 rounded-md border-gray-700 focus:border-violet-400" />
                     </div>
                     <div className="space-y-1 text-sm">
                         <label for="password" className="block dark:text-gray-800">Password</label>
